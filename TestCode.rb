7fbf4a5c22feb78bd8c27cc4f63968cb3817bf59
Perry Iler <piler1@my.westga.edu>
2020-01-17 19:54:20 -0500

    Added the point_for method to TuileBag

--- TestCode.rb
+++ TestCode.rb
@@ -1,7 +1,8 @@
 class TileBag  
-
+  
+     
   def initialize
-    @tiles = []
+    @tiles = Array.new
     9.times {@tiles.push(:A)}
     2.times {@tiles.push(:B)}
     2.times {@tiles.push(:C)}
@@ -35,6 +36,18 @@ class TileBag
     return tile
   end
   
+   def self.points_for(tile)
+    points = {
+      E: 1, A: 1, I: 1, O: 1, N: 1, R: 1, T: 1, L: 1, S: 1, U: 1,
+      D: 2, G: 2,
+      B: 3, C: 3, M: 3, P: 3,
+      F: 4, H: 4, V: 4, W: 4, Y: 4,
+      K: 5,
+      J: 8, X: 8, Q: 10, Z: 10      
+    }
+    points[tile]
+  end
+  
   def empty?
     @tiles.empty?
   end
@@ -47,6 +60,9 @@ end
 test = TileBag.new()
 puts test.empty?()
 puts test.getLength()
-puts test.draw_tile()
+drwTile = test.draw_tile()
+puts drwTile
 puts test.getLength()
+puts TileBag.points_for(drwTile)
+
 
